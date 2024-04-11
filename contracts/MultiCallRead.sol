94140db163cbacffeace705fa46ac8d9fc26ced0
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2024-04-10 20:56:23 -0700

    update multicall reader

--- contracts/MultiCallRead.sol
+++ contracts/MultiCallRead.sol
@@ -3,13 +3,23 @@ pragma solidity ^0.8.12;
 
 contract MultiCallRead {
 
-    function multiCall(address[] calldata targets, bytes[] calldata callDatas) external view returns (bytes[] memory) {
-        bytes[] memory results = new bytes[](targets.length);
-        for (uint256 i = 0; i < targets.length; i++) {
-            (bool success, bytes memory result) = targets[i].staticcall(callDatas[i]);
-            require(success, "MultiCallRead: call failed");
+    struct Call3 {
+        address target;
+        bool allowFailure;
+        bytes callData;
+    }
+
+    function multiCall(Call3[] calldata calls) external view returns (bytes[] memory results) {
+        results = new bytes[](calls.length);
+        for (uint256 i = 0; i < calls.length; i++) {
+            (bool success, bytes memory result) = calls[i].target.staticcall(calls[i].callData);
+            if (!calls[i].allowFailure) {
+                require(success, "MultiCallRead: call failed");
+            }
             results[i] = result;
         }
         return results;
     }
+
+
 }
--- test/MultiCallTest.js
+++ test/MultiCallTest.js
@@ -16,21 +16,21 @@ describe("Multi", function () {
     const MultiCall = await ethers.getContractFactory("Multicall3");
     const multiCall = await MultiCall.deploy();
 
-    const MultiCallRead = await ethers.getContractFactory("Multicall3");
-    const multiCallRead = await MultiCall.deploy();
+    const MultiCallRead = await ethers.getContractFactory("MultiCallRead");
+    const multiCallRead = await MultiCallRead.deploy();
 
     return { testContract, multiCall, multiCallRead, owner, otherAccount };
   }
 
   describe("Deployment", function () {
     it("Read the functions timestamps", async function () {
-      const { testContract, multiCall, owner } = await loadFixture(
+      const { testContract } = await loadFixture(
         deployContracts
       );
-      let function1Data = await testContract.func1();
-      console.log("function1Data", function1Data);
-      let function2Data = await testContract.func2();
-      console.log("function2Data", function2Data);
+      // let function1Data = await testContract.func1();
+      // console.log("function1Data", function1Data);
+      // let function2Data = await testContract.func2();
+      // console.log("function2Data", function2Data);
 
 
       await testContract.func3();
@@ -47,13 +47,13 @@ describe("Multi", function () {
   });
 
   describe("MultiCallTest", function () {
-    it("Test with MultiCall", async function () {
-      const { testContract, multiCall, owner } = await loadFixture(
+    it("Test with MultiCallRead", async function () {
+      const { testContract, multiCall, multiCallRead, owner } = await loadFixture(
         deployContracts
       );
 
-      // const callActions = ["func1", "func2", "func3", "func4"]; // Function names to be called
-      const callActions = ["func1"]; // Function names to be called
+      //const callActions = ["func1", "func2", "func3", "func4"]; // Function names to be called
+      const callActions = ["func1", "func2"]; // Function names to be called
       const callsData = await Promise.all(
         callActions.map(async (funcName) => {
           return testContract.interface.encodeFunctionData(funcName, []);
@@ -74,14 +74,58 @@ describe("Multi", function () {
       );
 
       try {
-        // const returnData = await multiCall.aggregate3(calls);
-         const returnData = await multiCall.staticAggregate3(calls);
-
+        const returnData = await multiCallRead.multiCall(calls);
           console.log("Return Data:", returnData);
        } catch (error) {
          console.error("Error executing aggregate3 call:", error);
        }
+    });
 
+    it("Test with MultiCall3 Function Call", async function () {
+      const { testContract, multiCall, multiCallRead, owner } = await loadFixture(
+        deployContracts
+      );
+
+      await testContract.func3();
+      await testContract.func4();
+      console.log("******Timestamp Called with a Normal Tx******")
+      console.log(
+        "Normal Call of Function 3:",
+        await testContract.timestampf3()
+      );
+      console.log(
+        "Normal Call of Function 4:",
+        await testContract.timestampf4()
+      );
+
+      const callActions = ["func3", "func4"]; // Function names to be called
+      const callsData = await Promise.all(
+        callActions.map(async (funcName) => {
+          return testContract.interface.encodeFunctionData(funcName, []);
+        })
+      );
+
+      let contractAddress = await testContract.target;
+
+      // Construct the calls array in the desired format
+      const calls = callsData.map((callData) => [
+        contractAddress,
+        true,
+        callData,
+      ]);
+      // console.log(
+      //   "Formatted batch calls for aggregate3:",
+      //   JSON.stringify(calls)
+      // );
+
+      try {
+         const returnData = await multiCall.aggregate3(calls);
+         // console.log("Return Data:", returnData);
+       } catch (error) {
+         console.error("Error executing aggregate3 call:", error);
+       }
+
+      console.log("******Timestamp Called with MultiCall3******")
        console.log(
         "Timestamp after Function 3:",
         await testContract.timestampf3()
@@ -91,6 +135,7 @@ describe("Multi", function () {
         await testContract.timestampf4()
       );
 
+      console.log("Data from MultiCall3:", returnData);
 
     });
   });
